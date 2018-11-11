6c07a5eefd5ec243fd05b4a8c5b39d88106a1377
unknown <psyouloveme>
2018-11-11 13:24:59 -0800

    Scaffold dotnet core project, add vscode files.

---
+++ AuthHelper.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace spotify_backup
+{
+    public class AuthHelper
+    {
+        private readonly string _client_id;
+        private readonly string _client_secret;
+        public AuthHelper(string clientId, string clientSecret)
+        {
+
+        }
+    }
+}
